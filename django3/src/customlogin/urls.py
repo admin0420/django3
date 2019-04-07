ce75fabe0c419612a0a03e2f726eea10ba898644
405-5 <405-5@405-5-PC>
2019-04-07 11:44:01 +0900

    첫 프로젝트 업로드

---
+++ django3/src/customlogin/urls.py
@@ -0,0 +1,16 @@
+'''
+Created on 2019. 3. 31.
+
+@author: 405-5
+'''
+from customlogin.views import *
+from django.urls.conf import path
+app_name='cl'
+urlpatterns = [
+    #127.0.0.1:8000/cl/signup
+    path('signup/', signup, name='signup'),
+   #127.0.0.1:8000/cl/signin
+    path('signin/', signin, name='signin'),
+    #127.0.0.1:8000/cl/signout
+    path('signout/', signout, name='signout')
+    ]
